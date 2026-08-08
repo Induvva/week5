920b060a9b515f313543eeb6cb82471645728e31
Indu <duvvaindu@gmail.com>
2026-08-08 14:31:13 +0530

    Fixed factorial program

--- Factorial.java
+++ Factorial.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Factorial {
 
     
-    public static void factorial(String[] args) {
+    public static int factorial(int n) {
         if(n==0||n==1){
             return 1;
         }
