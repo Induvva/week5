e2983f452bb6f24d81da0c12d3e631af479e927b
Indu <duvvaindu@gmail.com>
2026-08-08 14:40:19 +0530

    Fixed factorial program

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Factorial {
-
+    public class main(String[] args){
+        System.out.println("Factorail");
+    }
     
     public static int factorial(int n) {
         if(n==0||n==1){
